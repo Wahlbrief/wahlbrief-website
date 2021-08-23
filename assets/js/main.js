5225219530a047772c4487ae7ecd6bacb3c2d650
Anxhelo Lushka <anxhelo1995@gmail.com>
2021-08-23 18:19:09 +0200

    Token expired, reverted to local JSON

--- assets/js/main.js
+++ assets/js/main.js
@@ -394,8 +394,7 @@ copyButtons.forEach((btn) => {
 // Function to get items from JSON and build array
 
 (async () => {
-  const endpoint =
-    "https://raw.githubusercontent.com/Wahlbrief/wahlbrief-website/main/assets/test.json?token=AF6YQ53UUM435YQKHYQLRGDBEO2OE";
+  const endpoint = "/assets/test/json";
   const result = await fetch(endpoint).then((blob) => blob.json());
 
   const sorted = result.sort((a, b) => {
